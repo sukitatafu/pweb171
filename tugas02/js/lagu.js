637576e3b2094f7cafef57f4ed129e8043d3a114
sukitatafu <33944394+sukitatafu@users.noreply.github.com>
2017-12-07 14:27:17 +0800

    pembaharuan
    baru sadar kesalahaan

---
+++ tugas02/js/lagu.js
@@ -0,0 +1,13 @@
+counter = {
+  'pelangi':0,
+  'balon': 0,
+  'kereta': 0
+ }
+
+
+   function tampilLagu( id ){
+   $(".bloklagu").hide();
+   $('#'+id).show(); }
+
+   counter[id]++;
+  $('#c'+id).html(counter[id]); 
