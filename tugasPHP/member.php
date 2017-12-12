57877fb3fa76ff6341df77c2a8512a8173d409e9
sukitatafu <33944394+sukitatafu@users.noreply.github.com>
2017-12-12 17:15:17 +0800

    commit php
    masih salah di prostu.php baris ke 10 dak thu apa yang salah

---
+++ tugasPHP/member.php
@@ -0,0 +1,43 @@
+<?php
+/**
+ *
+ */
+class member
+{
+  private $nama;
+  private $email;
+  private $sex;
+  private $warna;
+
+  private $data =[
+    '51015003'=>['nama'=>'Aprianto', 'email'=>'Aprianto@kharisma.ac.id', 'sex'=>'lk' ,'warna'=>''],
+    '51015004'=>['nama'=>'Billy Tanyawan', 'email'=>'Biilytanyawan@kharisma.ac.id', 'sex'=>'lk' ,'warna'=>''],
+    '51015006'=>['nama'=>'Evelyn Wijaya', 'email'=>'Evelyn@kharisma.ac.id', 'sex'=>'pr' ,'warna'=>''],
+    '51015012'=>['nama'=>'Kenny Ivan', 'email'=>'kennyivan@kharisma.ac.id', 'sex'=>'lk' ,'warna'=>''],
+    '51015010'=>['nama'=>'Ivan Dharmawan', 'email'=>'ivandarmawan@kharisma.ac.id', 'sex'=>'lk' ,'warna'=>''],
+    '51015008'=>['nama'=>'Florencia Irene', 'email'=>'florencia@kharisma.ac.id', 'sex'=>'pr' ,'warna'=>''],
+  ];
+
+  function member($nim){
+  $this->nama =$this->data[$nim]['nama'];
+  $this->email=$this->data[$nim]['email'];
+  $this->sex=$this->data['sex'];
+  $this->warna=$this-data['warna'];
+  }
+
+  function getnama(){
+    return $this->nama;
+  }
+  function getemail(){
+    return $this->email;
+  }
+  function getsex(){
+    return $this->sex;
+  }
+  function getwarna(){
+    return $this->warna;
+  }
+
+}
+
+ ?>
---
+++ tugasPHP/prostu.php
@@ -0,0 +1,31 @@
+<?php
+  /**
+   *
+   */
+  class prostu
+  {
+      private $nama;
+      private $program;
+
+      private $data[
+        '51015003'=>['nama'=>'Sistem Informasi','program'=>'Diploma'],
+        '51015004'=>['nama'=>'Sistem Informasi','program'=>'Sarjana'],
+        '51015006'=>['nama'=>'Sistem Informasi','progrma'=>'sarjana'],
+        '51015012'=>['nama'=>'Sistem Informasi','progrma'=>'sarjana'],
+        '51015010'=>['nama'=>'Sistem Informasi','progrma'=>'sarjana'],
+        '51015008'=>['nama'=>'Sistem Informasi', 'progrma'=>'sarjana'],
+      ]  ;
+      function prostu($nim){
+        $this->nama = $this->data[$nim]['nama'];
+        $this->nama = $this->data[$nim]['program'];
+      }
+
+      function getnama(){
+        return $this->nama;
+      }
+      function getprogram(){
+        return $this->program;
+      }
+  }
+  
+ ?>
