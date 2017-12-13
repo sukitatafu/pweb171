806fef8c9d92ae321b1cd636fee7817c6084fa3f
sukitatafu <33944394+sukitatafu@users.noreply.github.com>
2017-12-13 15:09:26 +0800

    tugas pho.comit2
    selesai

--- tugasPHP/prostu.php
+++ tugasPHP/prostu.php
@@ -7,13 +7,13 @@
       private $nama;
       private $program;
 
-      private $data[
+      private $data=[
         '51015003'=>['nama'=>'Sistem Informasi','program'=>'Diploma'],
         '51015004'=>['nama'=>'Sistem Informasi','program'=>'Sarjana'],
         '51015006'=>['nama'=>'Sistem Informasi','progrma'=>'sarjana'],
         '51015012'=>['nama'=>'Sistem Informasi','progrma'=>'sarjana'],
         '51015010'=>['nama'=>'Sistem Informasi','progrma'=>'sarjana'],
-        '51015008'=>['nama'=>'Sistem Informasi', 'progrma'=>'sarjana'],
+        '51015008'=>['nama'=>'Sistem Informasi', 'progrma'=>'sarjana']
       ]  ;
       function prostu($nim){
         $this->nama = $this->data[$nim]['nama'];
@@ -27,5 +27,5 @@
         return $this->program;
       }
   }
-  
+
  ?>
