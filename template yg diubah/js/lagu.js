a1303a7aa2763360858b568acce23c48f5ecf6c0
sukitatafu <33944394+sukitatafu@users.noreply.github.com>
2017-12-14 17:07:49 +0800

    templet yang diubah
    hampirmi jadi tinggal sedikit

---
+++ template yg diubah/js/lagu.js
@@ -0,0 +1,16 @@
+counter = {
+  'pelangi':0,
+  'balon': 0,
+  'kereta': 0,
+  'cicak':0,
+  'kebun' :0
+ }
+   function tampilLagu( id ){
+   $('.bloklagu').hide();
+   $('#'+id).show();
+   counter[id]++;
+  $('#c'+id).html(counter[id]);
+
+  document.getElementById('info').style.display = "none";
+
+}
